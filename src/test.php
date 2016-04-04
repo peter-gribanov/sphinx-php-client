9dae15d1687a0947743d95e123e7b9214d76e3e2
Peter Gribanov <wcode404@gmail.com>
2016-04-04 18:43:46 +0300

    correct cody style

--- src/test.php
+++ src/test.php
@@ -63,39 +63,67 @@ $sortexpr = '';
 $limit = 20;
 $ranker = SPH_RANK_PROXIMITY_BM25;
 $select = '';
-for($i=0; $i<count($args); $i++)
-{
+for($i = 0; $i < count($args); $i++) {
 	$arg = $args[$i];
 
-	if ($arg=='-h' || $arg=='--host')				$host = $args[++$i];
-	elseif ($arg=='-p' || $arg=='--port')		$port = (int)$args[++$i];
-	elseif ($arg=='-i' || $arg=='--index')		$index = $args[++$i];
-	elseif ($arg=='-s' || $arg=='--sortby')		{ $sortby = $args[++$i]; $sortexpr = ''; }
-	elseif ($arg=='-S' || $arg=='--sortexpr')	{ $sortexpr = $args[++$i]; $sortby = ''; }
-	elseif ($arg=='-a' || $arg=='--any')			$mode = SPH_MATCH_ANY;
-	elseif ($arg=='-b' || $arg=='--boolean')		$mode = SPH_MATCH_BOOLEAN;
-	elseif ($arg=='-e' || $arg=='--extended')	$mode = SPH_MATCH_EXTENDED;
-	elseif ($arg=='-e2')							$mode = SPH_MATCH_EXTENDED2;
-	elseif ($arg=='-ph'|| $arg=='--phrase')		$mode = SPH_MATCH_PHRASE;
-	elseif ($arg=='-f' || $arg=='--filter')		$filter = $args[++$i];
-	elseif ($arg=='-v' || $arg=='--value')		$filtervals[] = $args[++$i];
-	elseif ($arg=='-g' || $arg=='--groupby')		$groupby = $args[++$i];
-	elseif ($arg=='-gs'|| $arg=='--groupsort')	$groupsort = $args[++$i];
-	elseif ($arg=='-d' || $arg=='--distinct')	$distinct = $args[++$i];
-	elseif ($arg=='-l' || $arg=='--limit')		$limit = (int)$args[++$i];
-	elseif ($arg=='--select')					$select = $args[++$i];
-	elseif ($arg=='-fr'|| $arg=='--filterrange')	$cl->setFilterRange($args[++$i], $args[++$i], $args[++$i]);
-	elseif ($arg=='-r')
-	{
+	if ($arg == '-h' || $arg=='--host') {
+		$host = $args[++$i];
+	} elseif ($arg == '-p' || $arg == '--port') {
+		$port = (int)$args[++$i];
+	} elseif ($arg == '-i' || $arg == '--index') {
+		$index = $args[++$i];
+	} elseif ($arg == '-s' || $arg == '--sortby') {
+		$sortby = $args[++$i];
+		$sortexpr = '';
+	} elseif ($arg == '-S' || $arg == '--sortexpr') {
+		$sortexpr = $args[++$i];
+		$sortby = '';
+	} elseif ($arg == '-a' || $arg == '--any') {
+		$mode = SPH_MATCH_ANY;
+	} elseif ($arg == '-b' || $arg == '--boolean') {
+		$mode = SPH_MATCH_BOOLEAN;
+	} elseif ($arg == '-e' || $arg == '--extended') {
+		$mode = SPH_MATCH_EXTENDED;
+	} elseif ($arg == '-e2') {
+		$mode = SPH_MATCH_EXTENDED2;
+	} elseif ($arg == '-ph'|| $arg == '--phrase') {
+		$mode = SPH_MATCH_PHRASE;
+	} elseif ($arg == '-f' || $arg == '--filter') {
+		$filter = $args[++$i];
+	} elseif ($arg == '-v' || $arg == '--value') {
+		$filtervals[] = $args[++$i];
+	} elseif ($arg == '-g' || $arg == '--groupby') {
+		$groupby = $args[++$i];
+	} elseif ($arg == '-gs'|| $arg == '--groupsort') {
+		$groupsort = $args[++$i];
+	} elseif ($arg == '-d' || $arg == '--distinct') {
+		$distinct = $args[++$i];
+	} elseif ($arg == '-l' || $arg == '--limit') {
+		$limit = (int)$args[++$i];
+	} elseif ($arg == '--select') {
+		$select = $args[++$i];
+	} elseif ($arg == '-fr' || $arg == '--filterrange') {
+		$cl->setFilterRange($args[++$i], $args[++$i], $args[++$i]);
+	} elseif ($arg == '-r') {
 		$arg = strtolower($args[++$i]);
-		if ($arg=='bm25')		$ranker = SPH_RANK_BM25;
-		if ($arg=='none')		$ranker = SPH_RANK_NONE;
-		if ($arg=='wordcount')$ranker = SPH_RANK_WORDCOUNT;
-		if ($arg=='fieldmask')$ranker = SPH_RANK_FIELDMASK;
-		if ($arg=='sph04')	$ranker = SPH_RANK_SPH04;
-	}
-	else
+		if ($arg == 'bm25') {
+			$ranker = SPH_RANK_BM25;
+		}
+		if ($arg == 'none') {
+			$ranker = SPH_RANK_NONE;
+		}
+		if ($arg == 'wordcount') {
+			$ranker = SPH_RANK_WORDCOUNT;
+		}
+		if ($arg == 'fieldmask') {
+			$ranker = SPH_RANK_FIELDMASK;
+		}
+		if ($arg == 'sph04') {
+			$ranker = SPH_RANK_SPH04;
+		}
+	} else {
 		$q .= $args[$i] . ' ';
+	}
 }
 
 ////////////
@@ -106,13 +134,27 @@ $cl->setServer($host, $port);
 $cl->setConnectTimeout(1);
 $cl->setArrayResult(true);
 $cl->setMatchMode($mode);
-if (count($filtervals))	$cl->setFilter($filter, $filtervals);
-if ($groupby)				$cl->setGroupBy($groupby, SPH_GROUPBY_ATTR, $groupsort);
-if ($sortby)				$cl->setSortMode(SPH_SORT_EXTENDED, $sortby);
-if ($sortexpr)			$cl->setSortMode(SPH_SORT_EXPR, $sortexpr);
-if ($distinct)			$cl->setGroupDistinct($distinct);
-if ($select)				$cl->setSelect($select);
-if ($limit)				$cl->setLimits(0, $limit,($limit>1000) ? $limit : 1000);
+if (count($filtervals)) {
+	$cl->setFilter($filter, $filtervals);
+}
+if ($groupby) {
+	$cl->setGroupBy($groupby, SPH_GROUPBY_ATTR, $groupsort);
+}
+if ($sortby) {
+	$cl->setSortMode(SPH_SORT_EXTENDED, $sortby);
+}
+if ($sortexpr) {
+	$cl->setSortMode(SPH_SORT_EXPR, $sortexpr);
+}
+if ($distinct) {
+	$cl->setGroupDistinct($distinct);
+}
+if ($select) {
+	$cl->setSelect($select);
+}
+if ($limit) {
+	$cl->setLimits(0, $limit,($limit>1000) ? $limit : 1000);
+}
 $cl->setRankingMode($ranker);
 $res = $cl->query($q, $index);
 
@@ -120,39 +162,34 @@ $res = $cl->query($q, $index);
 // print me out
 ////////////////
 
-if ($res===false)
-{
+if ($res === false) {
 	print 'Query failed: ' . $cl->getLastError() . ".\n";
 
-} else
-{
-	if ($cl->getLastWarning())
+} else {
+	if ($cl->getLastWarning()) {
 		print 'WARNING: ' . $cl->getLastWarning() . "\n\n";
+	}
 
 	print "Query '$q' retrieved $res[total] of $res[total_found] matches in $res[time] sec.\n";
 	print "Query stats:\n";
-	if (is_array($res['words']))
-		foreach($res['words'] as $word => $info)
+	if (is_array($res['words'])) {
+		foreach ($res['words'] as $word => $info) {
 			print "    '$word' found $info[hits] times in $info[docs] documents\n";
+		}
+	}
 	print "\n";
 
-	if (is_array($res['matches']))
-	{
+	if (is_array($res['matches'])) {
 		$n = 1;
 		print "Matches:\n";
-		foreach($res['matches'] as $docinfo)
-		{
+		foreach ($res['matches'] as $docinfo) {
 			print "$n. doc_id=$docinfo[id], weight=$docinfo[weight]";
-			foreach($res['attrs'] as $attrname => $attrtype)
-			{
+			foreach ($res['attrs'] as $attrname => $attrtype) {
 				$value = $docinfo['attrs'][$attrname];
-				if ($attrtype==SPH_ATTR_MULTI || $attrtype==SPH_ATTR_MULTI64)
-				{
+				if ($attrtype == SPH_ATTR_MULTI || $attrtype == SPH_ATTR_MULTI64) {
 					$value = '(' . join(',', $value) .')';
-				} else
-				{
-					if ($attrtype==SPH_ATTR_TIMESTAMP)
-						$value = date('Y-m-d H:i:s', $value);
+				} elseif ($attrtype == SPH_ATTR_TIMESTAMP) {
+					$value = date('Y-m-d H:i:s', $value);
 				}
 				print ", $attrname=$value";
 			}
