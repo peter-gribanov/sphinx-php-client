0dda59f267a90007c5ecc9978f9168b24ce1bfc9
Peter Gribanov <wcode404@gmail.com>
2016-04-04 19:13:38 +0300

    use autoload

--- src/test.php
+++ src/test.php
@@ -6,6 +6,12 @@
 
 $file = __DIR__.'/../vendor/autoload.php';
 
+if (!file_exists($file)) {
+    throw new RuntimeException('Install dependencies to run test suite. "php composer.phar install --dev"');
+}
+
+require_once __DIR__.'/../vendor/autoload.php';
+
 //////////////////////
 // parse command line
 //////////////////////
