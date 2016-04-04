cceef26c422616a5e15ec38c0662c76bdd67fd9f
Peter Gribanov <wcode404@gmail.com>
2016-04-04 18:35:29 +0300

    correct test code

--- tests/functional/build_excerpts.php
+++ tests/functional/build_excerpts.php
@@ -6,40 +6,43 @@
 
 $file = __DIR__.'/../../vendor/autoload.php';
 
+if (!file_exists($file)) {
+    throw new RuntimeException('Install dependencies to run test suite. "php composer.phar install --dev"');
+}
+
+require_once __DIR__.'/../../vendor/autoload.php';
+
 $docs = array
 (
-	'this is my test text to be highlighted, and for the sake of the testing we need to pump its length somewhat',
-	'another test text to be highlighted, below limit',
-	'test number three, without phrase match',
-	'final test, not only without phrase match, but also above limit and with swapped phrase text test as well',
+    'this is my test text to be highlighted, and for the sake of the testing we need to pump its length somewhat',
+    'another test text to be highlighted, below limit',
+    'test number three, without phrase match',
+    'final test, not only without phrase match, but also above limit and with swapped phrase text test as well',
 );
 $words = 'test text';
 $index = 'test1';
 $opts = array
 (
-	'before_match'		=> '<b>',
-	'after_match'		=> '</b>',
-	'chunk_separator'	=> ' ... ',
-	'limit'				=> 60,
-	'around'			=> 3,
+    'before_match'    => '<b>',
+    'after_match'     => '</b>',
+    'chunk_separator' => ' ... ',
+    'limit'           => 60,
+    'around'          => 3,
 );
-foreach ( array(0,1) as $exact )
-{
-	$opts['exact_phrase'] = $exact;
-	print "exact_phrase=$exact\n";
-	$cl = new SphinxClient ();
-	$res = $cl->buildExcerpts ( $docs, $index, $words, $opts );
-	if ( !$res )
-	{
-		die ( 'ERROR: ' . $cl->getLastError() . ".\n" );
-	} else
-	{
-		$n = 0;
-		foreach ( $res as $entry )
-		{
-			$n++;
-			print "n=$n, res=$entry\n";
-		}
-		print "\n";
-	}
+
+foreach (array(0, 1) as $exact) {
+    $opts['exact_phrase'] = $exact;
+    print 'exact_phrase=' . $exact . PHP_EOL;
+    $cl = new SphinxClient();
+    $res = $cl->buildExcerpts($docs, $index, $words, $opts);
+    if (!$res) {
+        exit(sprintf('ERROR: %s.' . PHP_EOL, $cl->getLastError()));
+    } else {
+        $n = 0;
+        foreach ($res as $entry) {
+            $n++;
+            printf('n=%s, res=%s' . PHP_EOL, $n, $entry);
+        }
+        print PHP_EOL;
+    }
 }
